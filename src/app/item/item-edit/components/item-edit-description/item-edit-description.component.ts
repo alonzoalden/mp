0441223868a1f51f22bac20192572768c99416bd
Edward Wang <wangzichen@toolots.com>
2019-09-25 08:41:24 +0800

    code regression

--- src/app/item/item-edit/components/item-edit-description/item-edit-description.component.ts
+++ src/app/item/item-edit/components/item-edit-description/item-edit-description.component.ts
@@ -64,6 +64,7 @@ export class ItemEditDescriptionComponent implements OnInit, AfterViewInit {
     }
 
     ngAfterViewInit() {
+
         var self = this;
 
             $('.summernote').summernote( {
@@ -83,11 +84,51 @@ export class ItemEditDescriptionComponent implements OnInit, AfterViewInit {
                     onBlur: function() {
                         self.updateTextEditorFields();
                     },
-                    onPaste: function (e) {
-                      var bufferText = ((e.originalEvent || e).clipboardData ).getData('Text/plain');
-                      e.preventDefault ? e.preventDefault() : (e.returnValue = false);
-                      document.execCommand('insertText', false, bufferText);
-                    }
+                    // onPaste: function (e) {
+                    //   var bufferText = ((e.originalEvent || e).clipboardData ).getData('text/html');
+                    //   e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+                    //   var stringStripper = /(\n|\r|Mso[a-zA-Z]+(")?)/g;
+                    //   var div = $('<div />');
+                    //   div.append(bufferText);
+                    //   div.find('*').removeAttr('style');
+                    //   div.find('*').removeAttr('class');
+                    //   console.log(div.html())
+                    //   setTimeout(function(){
+                    //     document.execCommand('insertHtml',false,div.html());
+                    //   },10);
+                    // }
+                  // onPaste:function (e) {
+                  //   var bufferText = ((e.originalEvent || e).clipboardData ).getData('text/html');
+                  //   console.log(bufferText);
+                  //   e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+                  //     // 1. remove line breaks / Mso classes
+                  //     var stringStripper = /(\n|\r| class=(")?Mso[a-zA-Z]+(")?)/g;
+                  //     var output = bufferText.replace(stringStripper, ' ');
+                  //     // 2. strip Word generated HTML comments
+                  //     var commentSripper = new RegExp('<!--(.*?)-->','g');
+                  //     var output = output.replace(commentSripper, '');
+                  //     var tagStripper = new RegExp('<(/)*(meta|link|span|\\?xml:|st1:|o:|font)(.*?)>','gi');
+                  //     // 3. remove tags leave content if any
+                  //     output = output.replace(tagStripper, '');
+                  //     // 4. Remove everything in between and including tags '<style(.)style(.)>'
+                  //     var badTags = ['style', 'script','applet','embed','noframes','noscript'];
+                  //
+                  //     for (var i=0; i< badTags.length; i++) {
+                  //       tagStripper = new RegExp('<'+badTags[i]+'.*?'+badTags[i]+'(.*?)>', 'gi');
+                  //       output = output.replace(tagStripper, '');
+                  //     }
+                  //     // 5. remove attributes ' style="..."'
+                  //     var badAttributes = ['style', 'start'];
+                  //     for (var i=0; i< badAttributes.length; i++) {
+                  //       var attributeStripper = new RegExp(' ' + badAttributes[i] + '="(.*?)"','gi');
+                  //       output = output.replace(attributeStripper, '');
+                  //     }
+                  //     console.log(output)
+                  //   setTimeout(function () {
+                  //     //this kinda sucks, but if you don't do a setTimeout,
+                  //     //the function is called before the text is really pasted.
+                  //     document.execCommand('insertHtml',false,output.html());
+                  //   }, 10);}
                 }
             });
             $('#itemDescriptionId').summernote('code', this.item.Description);
